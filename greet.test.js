a83b34dbbf9820385d0aa97827a190065580a41c
jirawat <jirawat_pao@hotmail.co.th>
2021-07-29 11:39:05 +0700

    add new test case given "Pao" want "Hello, Pao."

--- greet.test.js
+++ greet.test.js
@@ -8,3 +8,12 @@ test('given "Bob" want "Hello, Bob."', () => {
   
   expect(got).toBe(want); // Assert
 })
+
+test('given "Pao" want "Hello, Pao."', () => {
+  const given = 'Pao'
+  const want = 'Hello, Pao.' // Arrange
+
+  const got = greet(given) // Act
+
+  expect(got).toBe(want) // Assert
+})
